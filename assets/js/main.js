030a466c32d4d5c7b45bab21cba06683d45debad
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-20 15:45:56 +0200

    members markUp

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,12 +12,12 @@ const teamMembers = [
     image: 'angela-caroll-chief-editor.jpg'
 },
 {
-    name: 'Waleter gordon',
+    name: 'Walter gordon',
     role: 'Chief Editor',
     image: 'walter-gordon-office-manager.jpg'
 },
 {
-    name: 'Waleter gordon',
+    name: 'Walter gordon',
     role: 'Social Media Manager',
     image: 'angela-lopez-social-media-manager.jpg'
 },
@@ -31,6 +31,28 @@ const teamMembers = [
     role: 'Grafic designer',
     image: 'barbara-ramos-graphic-designer.jpg'
 }
-]
+];
 
+// Stampare su console le informazioni di nome, ruolo e la stringa della foto
 console.log(teamMembers);
+
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+for(let i = 0; i < teamMembers.length; i++){
+    const member = teamMembers[i];
+    console.log(member);
+
+    console.log(member.name);
+    console.log(member.role);
+    console.log(member.image);
+
+    const membersMarkup = 
+    `<div class="col-4 g-4">
+        <h4>${member.name}</h4>
+        <p>${member.role}</p>
+        <span>${member.image}</span>
+    </div>`;
+
+    document.querySelector('.row').insertAdjacentHTML('beforeend', membersMarkup);
+}
+
+
