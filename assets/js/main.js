53f74a158b5bb87cc3d1c5884f55f2408bdf8a58
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-20 15:14:17 +0200

    create array of members

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+// Creare l’array di oggetti con le informazioni fornite
+
+const teamMembers = [
+{
+    name: 'Wayne Barnett',
+    role: 'Founder & CEO',
+    image: 'wayne-barnett-founder-ceo.jpg'
+},
+{
+    name: 'Angela', 
+    role: 'Chief Editor',
+    image: 'angela-caroll-chief-editor.jpg'
+},
+{
+    name: 'Waleter gordon',
+    role: 'Chief Editor',
+    image: 'walter-gordon-office-manager.jpg'
+},
+{
+    name: 'Waleter gordon',
+    role: 'Social Media Manager',
+    image: 'angela-lopez-social-media-manager.jpg'
+},
+{
+    name: 'Scott Estrada',
+    role: 'Developer',
+    image: 'scott-estrada-developer.jpg'
+},
+{
+    name: 'Barbara Ramos',
+    role: 'Grafic designer',
+    image: 'barbara-ramos-graphic-designer.jpg'
+}
+]
+
+console.log(teamMembers);
